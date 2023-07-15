b3f552f7221088d138649ae2896a222c99661918
Michael Nguyen <michaeelnguyen@gmail.com>
2023-07-15 13:15:50 -0700

    Update App.test.js

--- frontend/r2d2-prototype-app/src/App.test.js
+++ frontend/r2d2-prototype-app/src/App.test.js
@@ -1,24 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from './App';
-import { render, screen, fireEvent } from '@testing-library/react';
 
+test('renders welcome message', () => {
+  render(<App />);
+  const welcomeMessage = screen.getByText("Welcome to the R2D2 Prototype!");
+  expect(welcomeMessage).toBeInTheDocument();
+});
 
-describe('App', () => {
-  test('renders welcome message', () => {
-    render(<App />);
-    const welcomeMessage = screen.getByText(/Welcome to the R2D2 Prototype/i);
-    expect(welcomeMessage).toBeInTheDocument();
-  });
+test('displays error message when prompt is not modified', () => {
+  process.env.REACT_APP_BACKEND_URL = 'http://localhost:8000/';
 
-  test('displays error message when prompt is not modified', () => {
-    render(<App />);
-    const submitButton = screen.getByText((content, element) => {
-      return element.tagName.toLowerCase() === 'button' && /Submit/i.test(content);
-    });
-    fireEvent.click(submitButton);
-    const errorMessage = screen.getByText(/Please modify the initial prompt./i);
-    expect(errorMessage).toBeInTheDocument();
-  });  
-
-  // Add more test cases as needed
+  render(<App />);
+  const submitButton = screen.getByText('Submit');
+  fireEvent.click(submitButton);
+  const errorMessage = screen.getByText('Please modify the initial prompt.');
+  expect(errorMessage).toBeInTheDocument();
 });
