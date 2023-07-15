d33d86d393aae1cd3587f2ecae13f8a4d670bb44
Michael Nguyen <michaeelnguyen@gmail.com>
2023-07-15 11:20:48 -0700

    Update App.test.js

--- frontend/r2d2-prototype-app/src/App.test.js
+++ frontend/r2d2-prototype-app/src/App.test.js
@@ -1,5 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders welcome message', () => {
   render(<App />);
   const welcomeMessage = screen.getByText("Welcome to the R2D2 Prototype!");
   expect(welcomeMessage).toBeInTheDocument();
 });
+
