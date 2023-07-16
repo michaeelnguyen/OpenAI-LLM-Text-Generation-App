1a4a2cfd13b5e644c3c978b80ebba8f9a9815ff5
Michael Nguyen <michaeelnguyen@gmail.com>
2023-07-16 09:22:52 -0700

    Merge dev branch to main

---
+++ backend/app/main.py
@@ -0,0 +1,121 @@
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from mangum import Mangum
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+
+# Create an instance of the FastAPI application
+app = FastAPI()
+
+# Configure CORS to connect to different port mappings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://r2d2-prototype-react-app.s3-website-us-east-1.amazonaws.com"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class InputData(BaseModel):
+    option: str
+    value: str
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve OpenAI API key from environment variable
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Check if the API key is set
+if openai_api_key is None:
+    raise ValueError('OpenAI API key is not set')
+
+# Set the OpenAI API key in the environment variable
+os.environ['OPENAI_API_KEY'] = openai_api_key
+
+@app.post("/")
+def process_data(input_data: InputData):
+    option = input_data.option
+    prompt = input_data.value
+
+    # Create an instance of the OpenAI API
+    openai = OpenAI(openai_api_key=openai_api_key)
+
+    if prompt and option == 'MarketGPT':
+        # Prompt templates defined for multiple task to obtain competitors research for specific company.
+        org_template = PromptTemplate(
+            input_variables=['company'], 
+            template= "Provide info about the company size, industry, key decision makers, and website for {company}."
+        )
+        finance_template = PromptTemplate(
+            input_variables=['company'], 
+            template= "Provide info about the revenue and profitability for {company}."
+        )
+        detailed_info_template = PromptTemplate(
+            input_variables=['company'], 
+            template= "Provide info about what {company} does and their product line."            
+        )
+        latest_news_template = PromptTemplate(
+            input_variables=['company'], 
+            template= "Provide the latest news about {company}"            
+        )
+        competitors_template = PromptTemplate(
+            input_variables=['company'], 
+            template= "Provide info about the competitors for {company}."         
+        )
+        # Define tasks (chains) that use openAI as LLM and the previously defined templates
+        org_chain = LLMChain(llm=openai, prompt=org_template, verbose=True, output_key='org')
+        finance_chain =  LLMChain(llm=openai, prompt=finance_template, verbose=True, output_key='finance')
+        detailed_info_chain = LLMChain(llm=openai, prompt=detailed_info_template, verbose=True, output_key='detailed_info')
+        latest_news_chain = LLMChain(llm=openai, prompt=latest_news_template, verbose=True, output_key='latest_news')
+        competitors_chain = LLMChain(llm=openai, prompt=competitors_template, verbose=True, output_key='competitors')
+
+        # Group all tasks together for sequential execution
+        sequential_chain = SequentialChain(
+            chains=[org_chain, finance_chain, detailed_info_chain, latest_news_chain, competitors_chain], 
+            input_variables = ['company'],
+            output_variables = ['org', 'finance', 'detailed_info', 'latest_news', 'competitors'],
+            verbose=True
+        )
+        responses = sequential_chain({'company': prompt})
+
+        # Create a dictionary to store the responses for each section
+        section_responses = {
+            'Organization': responses['org'],
+            'Finance': responses['finance'],
+            'Info and Products': responses['detailed_info'],
+            'Latest News': responses['latest_news'],
+            'Competitors': responses['competitors']
+        }
+        return {"value": section_responses}
+    
+    elif prompt and option == 'Personalized Email Outreach':
+        # Generate personalized email using user prompt
+        response = openai.predict(prompt)
+        return {"value": response}
+    elif prompt and option == 'Social Media Posting':
+        # Generate social media post using user prompt
+        response = openai.predict(prompt)
+        return {"value": response}
+    else:
+        return {"value": "Error"}
+
+@app.get("/")
+def get_root():
+    return {"message": "Hello. This is the backend!"}
+
+# Mangum is an ASGI adapter that acts as a compatibility layer between 
+# ASGI protocol and the event-driven AWS Lambda execution environment. 
+# This allows ASGI apps like FastAPI to take advantage serverless scalability,
+# costs and auto-scaling. The handler is defined in AWS Lambda Runtime settings 
+# as 'app.main.handler'.
+handler = Mangum(app)
---
+++ frontend/r2d2-prototype-app/src/App.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react';
+import { Ring } from '@uiball/loaders'
+import './App.css';
+
+function App() {
+  // Define the options for the use cases and their corresponding prompts
+  const options = [
+    { label: 'MarketGPT', prompt: 'Enter the company name to perform competitor research.' },
+    { label: 'Personalized Email Outreach', prompt: 'Generate email content for [Recipient] about [Subject].' },
+    { label: 'Social Media Posting', prompt: 'Create a Twitter post about [Topic].' }
+  ];
+
+  // State variable definitions
+  const [value, setValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [initialPrompt, setInitialPrompt] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [outputMsg, setOutputMsg] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  // Handle textArea change and resizing based user input
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleResize = (event) => {
+    event.target.style.height = 'auto';
+    event.target.style.height = event.target.scrollHeight + 'px';
+  };
+
+  // Handle when different feature is selected
+  const handleChoiceClick = (option) => {
+    setSelectedOption(option);
+    setValue(option.label === 'MarketGPT' ? '' : option.prompt);
+    setInitialPrompt(option.label === 'MarketGPT' ? '' : option.prompt);
+    setErrorMsg('');
+    setOutputMsg('');
+    setLoading(false);
+  };
+  
+
+  // Handle clear btn functionality to reset to default text values
+  const handleClear = () => {
+    setValue(selectedOption.label === 'MarketGPT' ? '' : initialPrompt);
+    setErrorMsg('');
+    setOutputMsg('');
+    setLoading(false);
+  };
+
+  // Handle submit btn functionality to send POST request of user input to FastAPI backend
+  const handleSubmit = async () => {
+    if (selectedOption) {
+      if (!value.trim() || value === initialPrompt) {
+        setErrorMsg('Please modify the initial prompt.');
+        return;
+      }
+
+      setLoading(true);
+
+      // Attempt to pass user input to the FastAPI backend
+      try {
+        const response = await fetch(process.env.REACT_APP_BACKEND_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            option: selectedOption.label,
+            value: value 
+          }),
+        });
+        // If FastAPI and Langchain API call are successful return the generated response
+        if (response.ok) {
+          const responseOutput = await response.json();
+          setOutputMsg(responseOutput.value);
+        } else {
+          console.error('Status Error:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  // Start of the main component render
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="app-info">
+          <h1>Welcome to the R2D2 Prototype!</h1>
+          <div className="app-choices">
+            {/* Define the use cases as buttons */}
+            {options.map((option) => (
+              <button
+                key={option.label}
+                className={`choice-btn ${selectedOption === option ? 'selected' : ''}`}
+                onClick={() => handleChoiceClick(option)}
+              >
+                {option.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      </header>
+      <main className="App-body">
+        {/* Display error message when user doesnt enter custom prompt */}
+        {errorMsg && (
+          <div className="error-msg">
+            {errorMsg}
+          </div>
+        )}
+        {/* Handle state of the textarea input and the Clear and Submit btns */}
+        {selectedOption && (
+          <section className="textbox">
+            <div className="prompt-input">
+              <textarea className="prompt-textarea" value={value} placeholder={selectedOption ? selectedOption.prompt : ''}
+                onChange={handleChange}
+                onInput={handleResize}
+              ></textarea>
+            </div>
+            <div className="btn-container">
+              <button className="clear-btn" onClick={handleClear}>
+                Clear
+              </button>
+              <button className="submit-btn" onClick={handleSubmit}>
+                Submit
+              </button>
+            </div>
+          </section>
+        )}
+        {/* Loading animation after user input submitted */}
+        {loading && <Ring size={35} color="#379da1" />}
+        {/* If response was sent back from the FastAPI backend, display output from OpenAI */}
+        {outputMsg && (
+          <div className="output-msg">
+            {selectedOption.label === 'MarketGPT' ? (
+              <div>
+                {/* Handle MarketGPT case separately since we return key, value pairs */}
+                {Object.entries(outputMsg).map(([section, response]) => (
+                <div key={section}>
+                  <h4>{section}</h4>
+                  <p>{response}</p>
+                </div>
+                ))}
+              </div>
+            ) : (
+              // Personalized email and social media posting should just return a string
+              <p>{outputMsg}</p>
+            )}
+          </div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
